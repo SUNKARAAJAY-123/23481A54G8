a277d08c084bcee54d1933189c2c9279ab06fee9
SUNKARA AJAY <sunkaraajay86@gamil>
2026-05-09 11:41:58 +0530

    add project files

---
+++ notification_app_fe/src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async () => {
+    alert("Notification Sent Successfully");
+
+    console.log({
+      title,
+      message,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>Notification App</h1>
+
+      <br />
+
+      <input
+        type="text"
+        placeholder="Enter Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{
+          width: "300px",
+          padding: "10px",
+        }}
+      />
+
+      <br />
+      <br />
+
+      <textarea
+        placeholder="Enter Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        style={{
+          width: "300px",
+          height: "100px",
+          padding: "10px",
+        }}
+      />
+
+      <br />
+      <br />
+
+      <button
+        onClick={handleSubmit}
+        style={{
+          padding: "10px 20px",
+          cursor: "pointer",
+        }}
+      >
+        Send Notification
+      </button>
+    </div>
+  );
+}
+
+export default App;
